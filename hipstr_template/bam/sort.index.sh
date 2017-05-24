f06f26a87ba6c1d2acf935541c1af89d37cc7bf4
shubhamsaini <shubhamsaini@gmail.com>
2017-05-24 11:18:47 -0700

    Added HipSTR regions

--- hipstr_template/bam/sort.index.sh
+++ hipstr_template/bam/sort.index.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 ls *.bam > files.list
-cat files.list | xargs -I% -n1 samtools sort % %
-mkdir sorted
-mv *.bam.bam sorted/
-rm *.bam
-mv sorted/*.bam .
-cat files.list | xargs -I% -n1 mv %.bam %
+cat files.list | xargs -I% -n1 samtools sort % -o %
 cat files.list | xargs -I% -n1 samtools index %
