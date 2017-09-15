6ebc71509b8439b9ca3f40d4de6c43b4496f6d30
Shubham Saini <shubhamsaini@gmail.com>
2017-09-15 14:15:14 -0700

    Changed L1O to use only parents as ref

--- L1O_Analysis/L1O.sh
+++ L1O_Analysis/L1O.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### usage: 
-### ./L1O.sh -s sampleID -p pedigree.txt -f samples.txt -l final.str.snp.vcf.gz -t hipstrcalls.vcf.gz -b path/to/beagle.exe
-### Eg: ./L1O.sh -s SSC00092 -p pedigree.txt -f sampleID.txt -l final.str.snp.vcf.gz -t str-vcf-paired-v2.vcf.gz -b beagle.27Jul16.86a.jar 
+### ./L1O.sh -s sampleID -p pedigree.fam -l final.str.snp.vcf.gz -t hipstrcalls.vcf.gz -b path/to/beagle
+### Eg: ./L1O.sh -s SSC00092 -p pedigree.fam -l final.str.snp.vcf.gz -t str-vcf-paired-v2.vcf.gz -b beagle.27Jul16.86a.jar 
 ###
 
 while getopts s:p:f:t:b:l: option
@@ -11,7 +11,6 @@ do
         in
                 s) sss=${OPTARG};;
                 p) ped=${OPTARG};;
-                f) samFile=$OPTARG;;
                 t) str=$OPTARG;;
                 b) beagle=$OPTARG;;
                 l) procStr=$OPTARG;;
@@ -20,18 +19,13 @@ done
 
 export SAMPLEID=$sss
 echo $SAMPLEID
-export FATHERID=`cat $ped | grep $SAMPLEID | awk '{print $2}' | head -1`
-export MOTHERID=`cat $ped | grep $SAMPLEID | awk '{print $3}' | head -1`
-export CHILD1=`cat $ped | grep $FATHERID | awk '{print $1}' | head -1`
-export CHILD2=`cat $ped | grep $FATHERID | awk '{print $1}' | tail -n 1`
+export famID=`cat $ped | grep $SAMPLEID | head -1 | awk '{print $1}'`
+export CHILD1=`cat $ped | awk -v var="$famID" '$1==var {print $0}' | awk '$3!=0 {print $0}' | head -1 | awk '{print $2}'`
+export CHILD2=`cat $ped | awk -v var="$famID" '$1==var {print $0}' | awk '$3!=0 {print $0}' | tail -n 1 | awk '{print $2}'`
+export FATHERID=`cat $ped | awk -v var="$famID" '$1==var {print $0}' | awk '$3!=0 {print $0}' | head -1 | awk '{print $3}'`
+export MOTHERID=`cat $ped | awk -v var="$famID" '$1==var {print $0}' | awk '$3!=0 {print $0}' | head -1 | awk '{print $4}'`
 
-rm sampleExclude.txt
-echo $FATHERID > sampleExclude.txt
-echo $MOTHERID >> sampleExclude.txt
-echo $CHILD1 >> sampleExclude.txt
-echo $CHILD2 >> sampleExclude.txt
-
-cat $samFile | grep -v $FATHERID | grep -v $MOTHERID | grep -v $CHILD1 | grep -v $CHILD2 > sampleRef.txt
+cat $ped | awk '$3==0 {print $2}' | grep -v $FATHERID | grep -v $MOTHERID | grep -v $CHILD1 | grep -v $CHILD2 > sampleRef.txt
 
 bcftools query -f '%ID\n' $str > ID.txt
 bcftools view $procStr --samples-file sampleRef.txt --output-type z --output-file ref.vcf.gz --force-samples
@@ -42,20 +36,13 @@ rm ref.vcf.gz
 mv ref.rem.vcf.gz ref.vcf.gz
 bcftools index -f ref.vcf.gz
 
-#java  -Xmx8g -jar $beagle gt=ref.vcf.gz out=ref.imputed
 java  -Xmx8g -jar $beagle gt=exclude.vcf.gz ref=ref.vcf.gz out=imputed
 bcftools index imputed.vcf.gz 
 bcftools view imputed.vcf.gz --include ID=@ID.txt > imputed.str
 
-#bcftools query -f '%CHROM:%POS\t[%TGT\t]\n' imputed.str | sort -V | sed 's/^chr//g' > $SAMPLEID.imputeResult.txt
-#bcftools query --samples $SAMPLEID -f '%CHROM:%POS\t[%TGT\t]\n' $str | sort -V | sed 's/^chr//g' > $SAMPLEID.groundTruth.txt
-
-rm $SAMPLEID.imputeResult.txt
-rm $SAMPLEID.groundTruth.txt
 python write_bases.py imputed.str $SAMPLEID $SAMPLEID.imputeResult.txt
 python write_bases.py $str $SAMPLEID $SAMPLEID.groundTruth.txt
 
-#sdiff imputeResult.txt groundTruth.txt 
 echo "done"
 
 rm exclude.vcf.gz ref.vcf.gz sampleExclude.txt sampleRef.txt ref.vcf.gz.csi imputed.vcf.gz imputed.vcf.gz.csi imputed.str
