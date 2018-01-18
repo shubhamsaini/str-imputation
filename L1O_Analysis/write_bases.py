fa2d934c124d9c105a01ea817a09f956e37ab87c
Shubham Saini <shubhamsaini@gmail.com>
2018-01-18 10:54:06 -0800

    L1O now uses difference from reference

--- L1O_Analysis/write_bases.py
+++ L1O_Analysis/write_bases.py
@@ -9,9 +9,10 @@ vcf = VCF(sys.argv[1], samples=sys.argv[2])
 file = open(sys.argv[3],'a')
 for v in vcf:
 	pos = ":".join([str(v.CHROM), str(v.POS)])
+	refLen = len(v.REF)
 	gt_bases = v.gt_bases[0]
 	gt_bases = re.split('/|\|',gt_bases)
-	gt_bases_len = [str(len(i)) for i in gt_bases]
+	gt_bases_len = [str(len(i) - refLen) for i in gt_bases]
 	final_list = [pos]+gt_bases_len+["\n"]
 	file.write("\t".join(final_list))
 file.close()
